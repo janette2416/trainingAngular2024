28be0f22139aa6d0a7958c39bef70da1dd6c41e2
Janette <janettemangubat2416@gmail.com>
2024-09-27 19:16:54 +0800

    section 1 to 3 setting up correctly

---
+++ projects/section-one-to-three/src/app/task-field/task-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-field.component.html',
+  styleUrl: './task-field.component.scss'
+})
+export class TaskFieldComponent {
+
+}
